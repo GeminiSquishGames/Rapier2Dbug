5dac268df00f7e03aed824e2452929e50aadeb91
Greenbox Games <lintphishx@gmail.com>
2024-04-23 14:53:14 -0500

    See rocks rotate better

--- toss_rock.gd
+++ toss_rock.gd
@@ -11,16 +11,21 @@ func _ready():
 
 
 func make_rock():
+    # dumb... just turn off timer if < max
     if num_rocks >= max_rocks:
         return
+
+    var rock_instance : RigidBody2D = rock.instantiate()
 
     var impulse_x = randf_range(-1000, 1000)
     var impulse_y = randf_range(-1000, 1000)
     var impulse_vec = Vector2(impulse_x,impulse_y) * 10
-    var rock_instance : RigidBody2D = rock.instantiate()
+    var rot_impulse = randf_range(-1000, 1000) * rock_instance.mass
     rock_instance.position = position
     # n toss
     rock_instance.apply_impulse(impulse_vec)
+    rock_instance.apply_torque_impulse(rot_impulse)
+
     get_tree().root.add_child(rock_instance)
 
     num_rocks += 1
